3baab55e251a74e113b6989e6970cee5a9cdbccd
Gabriel Zhang <gabrielzhang@itadmin-gabriel-MACBOOK-PRO.local>
2023-10-20 17:15:02 +0800

    -- init website

---
+++ src/pages/website/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <n-message-provider>
+    <div class="appContent">
+      <RouterView></RouterView>
+    </div>
+  </n-message-provider>
+</template>
+ 
+<script lang="ts">
+import { RouterView } from 'vue-router'
+import { NMessageProvider } from 'naive-ui'
+import "./app.scss";
+
+export default {
+  components: {
+    RouterView,
+    NMessageProvider
+  }
+}
+</script>
---
+++ src/pages/website/index.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue';
+import store from './store';
+import App from './App.vue';
+import axios from './http';
+import router from './router'
+
+const _create = () => {
+	const app = createApp(App);
+	app.use(store);
+	app.use(router);
+	app.mount('#rootApp');
+	app.config.globalProperties.$http = axios;
+}
+
+_create();
+
+if ((module as any).hot) {
+	console.log('开发环境，启用热加载更新！');
+	(module as any).hot.accept();
+}
