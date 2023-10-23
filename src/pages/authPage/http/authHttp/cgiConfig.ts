4d8ec594d785e648f4023e85dda71810693cc5a0
Gabriel Zhang <gabrielzhang@itadmin-gabriel-MACBOOK-PRO.local>
2023-10-23 16:48:23 +0800

    --

--- src/pages/authPage/http/authHttp/cgiConfig.ts
+++ src/pages/authPage/http/authHttp/cgiConfig.ts
@@ -4,19 +4,19 @@ import { cgiPath } from '../index'
 export const cgi_register = `${cgiPath}/api/user/register`
 
 // 登录
-export const cgi_login = `${cgiPath}/api/login/singIn`
+export const cgi_login = `${cgiPath}/api/user/login/singIn`
 
 // 退出
-export const cgi_signOut = `${cgiPath}/api/login/signOut`
+export const cgi_signOut = `${cgiPath}/api/user/login/signOut`
 
 // 图形验证码
-export const cgi_getCaptcha = `${cgiPath}/api/login/getCaptcha`
+export const cgi_getCaptcha = `${cgiPath}/api/user/login/getCaptcha`
 
 // 手机验证码
-export const cgi_sendVerify = `${cgiPath}/api/login/sendVerify`
+export const cgi_sendVerify = `${cgiPath}/api/user/login/sendVerify`
 
 // 重置密码
 export const cgi_resetPassword = `${cgiPath}/api/user/updatePasswordRequest`
 
 // 微信登录
-export const cgi_wxSingIn = `${cgiPath}/api/login/wxSingIn`
+export const cgi_wxSingIn = `${cgiPath}/api/user/login/wxSingIn`
